adce95c21b2efda63b1ff9c7c2f88328ce901cec
PhantomNero <116944623+PhantomNero@users.noreply.github.com>
2023-03-23 19:53:48 +0300

    23.03.2023

--- GoogleApps/level_2/your_code_2.py
+++ GoogleApps/level_2/your_code_2.py
@@ -2,10 +2,12 @@ import pandas as pd
 df = pd.read_csv('GoogleApps.csv')
 
 # Сколько стоит (Price) самое дешёвое платное приложение (Type == 'Paid)?
+print(df[df['Type'] == 'Paid']['Price'].min())
 
-
+print('*' * 100, '\n')
 # Чему равно медианное (median) количество установок (Installs)
 # приложений из категории (Category) "ART_AND_DESIGN"?
+print(df[df['Category'] == 'ART_AND_DESIGN']['Installs'].median())
 
 
 # На сколько максимальное количество отзывов (Reviews) для бесплатных приложений (Type == 'Free')
