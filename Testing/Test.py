8403e5244e6693739f6ee5cfb79cf87e48e74dff
PhantomNero <116944623+PhantomNero@users.noreply.github.com>
2023-03-27 16:56:21 +0300

    23.03.2023

--- Testing/Test.py
+++ Testing/Test.py
@@ -1,23 +1,5 @@
-# g g g g g п
-from tkinter import *
-from tkinter import messagebox
-import random
-def no():
-    messagebox.showinfo(' ', '+REP for -Z E R G- :)')
-    quit()
-def motionMouse(event):
-    btnn.place(x=random.randint(100, 500), y=random.randint(100, 300))
-root = Tk()
-root.geometry('600x400')
-x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
-y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
-root.wm_geometry("+%d+%d" % (x, y))
-root.title("Опрос от Steam.")
-root.resizable(width=False, height=False)
-root['bg'] = 'white'
-lable = Label(root, text='Вам нравится -Z E R G-?', font='Arial 20 bold', bg='white').pack()
-btnn = Button(root, text='Нет', font='Arial 20 bold')
-btnn.place(x=170, y=100)
-btnn.bind('<Enter>', motionMouse)
-btny = Button(root, text='Да', font='Arial 20 bold', command=no).place(x=350, y=100)
-root.mainloop()
+a = 400
+b = 175
+c = a/b
+c = round(c, 2)
+print(c)
