c164296aac79f3d03c38d1923df8c12529697054
PhantomNero <116944623+PhantomNero@users.noreply.github.com>
2023-04-16 15:05:37 +0300

    16.04.2023

--- GoogleApps/level_5/data_cleaning.py
+++ GoogleApps/level_5/data_cleaning.py
@@ -9,38 +9,57 @@ df = pd.read_csv('GooglePlayStore_wild.csv')
 # Преобразуй размеры приложений ('Size') в числовой формат (float). Размер всех приложений должен измеряться в Мегабайтах.
 # Чему равен максимальный размер ('Size') приложений из категории ('Category') 'TOOLS'?
 #print(df[df['Category'] == 'TOOLS']['Size'].max())
-
-pd.set_option('display.max_rows', 10840)
+#pd.set_option('display.max_rows', 10840)
+#pd.set_option('display.max_colwidth', 10840)
+#pd.set_option('display.max_columns', None)
 #print(a)
-tp = (df[(df["Category"] == "TOOLS")])["Size"].max()
+#tp = (df[(df["Category"] == "TOOLS")])["Size"].max()
 
-sm3 = df[(df["Category"] == "TOOLS") & (df["Category"] != "Varies with device")]["Size"].max()
+#sm3 = df[(df["Category"] == "TOOLS") & (df["Category"] != "Varies with device")]["Size"].max()
 
 #print(sm3)
 
 #print(df["Size"].value_counts())
 #print(tp)
 
-def set_size(size):
-    if size[-1] == "M":
-        return float(size[:-1])
-    if size[-1] == "k":
-        return float(size[:-1]) / 1024
-    return -1
+#def set_size(size):
+#    if size[-1] == "M":
+#       return float(size[:-1])
+#    if size[-1] == "k":
+#        return float(size[:-1]) / 1024
+#    return -1
 
-df["Size"] = df["Size"].apply(set_size).max()
+#df["Size"] = df["Size"].apply(set_size).max()
 #print(df["Size"].value_counts())
-print(df[df['Category'] == 'TOOLS']['Size'].max())
+#print(df[df['Category'] == 'TOOLS']['Size'].max())
 # Бонусные задания
 # Замени тип данных на целочисленный (int) для количества установок ('Installs').
 # В записи количества установок ('Installs') знак "+" необходимо игнорировать.
 # Т.е. если в датасете количество установок равно 1,000,000+, то необходимо изменить значение на 1000000
+#def set_installs():
+#    if installs == "0":
+#        return 0
+#    return int(installs[:-1].replace(',', ','))
 
+#df["Installs"] = df['Installs'].apply(set_installs())
+#print(df["Installs"])
+
+# int64   10840   Installs
+#
+#
+# 10839 10000000
 # Сгруппируй данные по категории ('Category') и целевой аудитории ('Content Rating') любым удобным для тебя способом,
 # посчитай среднее количество установок ('Installs') в каждой группе. Результат округли до десятых.
 # В полученной таблице найди ячейку с самым большим значением.
 # К какой возрастной группе и типу приложений относятся данные из этой ячейки?
-
+#result = df.pivot_table(index='Content Rating', columns='Type', values='Installs', aggfunc='mean')
+#print(round(result, 1))
 # У какого приложения не указан тип ('Type')? Какой тип там необходимо записать в зависимости от цены?
+#df["Type"].fillna("SSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS", inplace=True)
+#print(df.info)
+sm = (df[(df["Type"] != "Free") & (df["Type"] != "Paid")]["App"])
+sm2 = (df[(df["Type"] != "Free") & (df["Type"] != "Paid")]["Price"])
+print(sm)
+print(sm2)
 
 # Выведи информацию о всем DataFrame, чтобы убедиться, что очистка прошла успешно
--- GoogleApps/level_5/feature_engineering.py
+++ GoogleApps/level_5/feature_engineering.py
@@ -1,13 +1,35 @@
 import pandas as pd
+
 df = pd.read_csv('GooglePlayStore_wild.csv')
+
 
 # Очистка данных из первого задания
 
 # Замени тип данных на дробное число (float) для цен приложений ('Price')
 
 # Вычисли, сколько долларов разработчики заработали на каждом платном приложении
-
+# def split(genres):
+#    return genres.split(",")
+# df["Genres"] = df["Genres"].apply(split)
+# df["Numbres of genres"]
 # Чему равен максимальный доход ('Profit') среди платных приложений (Type == 'Paid')?
+def set_season(date):
+    month = date.sprit()[0]
+    seasons = {
+        "Зима": ["December", "January", "February"],
+        "Лето": ["June", "July", "August"],
+        "Весна": ["March", "April", "May"],
+        "Осень": ["October", "November", "September"]}
+
+    for season in seasons:
+        if month in seasons[season]:
+            return season
+        return "Сезон не установлен"
+
+
+df["Season"] = df["Last Updated"].apply(set_season)
+
+print(df["Season"].value_counts())
 
 # Создай новый столбец, в котором будет храниться количество жанров. Назови его 'Number of genres'
 
